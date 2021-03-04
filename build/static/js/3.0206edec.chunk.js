fe80b15666585e5ea7d54305feccd7c713082bfc
MaxProPlus <mutasik170300@gmail.com>
2021-03-04 19:24:51 +0400

    init

--- build/static/js/3.0206edec.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcra-test"]=this["webpackJsonpcra-test"]||[]).push([[3],{30:function(t,c,n){"use strict";n(0),n(31);var e=n(1);c.a=function(t){return Object(e.jsx)("button",{className:"btn ".concat(t.className||""),children:t.children})}},31:function(t,c,n){},32:function(t,c,n){},34:function(t,c,n){"use strict";n.r(c);n(0);var e=n(30),s=(n(32),n(1));c.default=function(t){return Object(s.jsxs)("div",{children:["page2",Object(s.jsx)(e.a,{className:"btn-page2",children:"\u041a\u043d\u043e\u043f\u043a\u0430"})]})}}}]);
-//# sourceMappingURL=3.0206edec.chunk.js.map
--- build/static/js/4.d35f7147.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcra-test"]=this["webpackJsonpcra-test"]||[]).push([[4],{30:function(t,c,n){"use strict";n(0),n(31);var e=n(1);c.a=function(t){return Object(e.jsx)("button",{className:"btn ".concat(t.className||""),children:t.children})}},31:function(t,c,n){},33:function(t,c,n){"use strict";n.r(c);n(0);var e=n(30),s=n(1);c.default=function(t){return Object(s.jsxs)("div",{children:["page1",Object(s.jsx)(e.a,{children:"\u041a\u043d\u043e\u043f\u043a\u0430"})]})}}}]);
-//# sourceMappingURL=4.d35f7147.chunk.js.map
