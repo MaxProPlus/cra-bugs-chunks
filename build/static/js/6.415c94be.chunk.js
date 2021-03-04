e7039ef611322aa64d4591552362ef5634071a8c
MaxProPlus <mutasik170300@gmail.com>
2021-03-04 19:23:30 +0400

    init

---
+++ build/static/js/6.415c94be.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcra-test"]=this["webpackJsonpcra-test"]||[]).push([[6],{30:function(t,c,n){"use strict";n(0),n(31);var e=n(1);c.a=function(t){return Object(e.jsx)("button",{className:"btn ".concat(t.className||""),children:t.children})}},31:function(t,c,n){},36:function(t,c,n){"use strict";n.r(c);n(0);var e=n(30),s=n(1);c.default=function(t){return Object(s.jsxs)("div",{children:["page4",Object(s.jsx)(e.a,{children:"\u041a\u043d\u043e\u043f\u043a\u0430"})]})}}}]);
+//# sourceMappingURL=6.415c94be.chunk.js.map
